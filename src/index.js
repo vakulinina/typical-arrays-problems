5eb557ceff25c7aaf61d05604c3f8e2b75fd93e1
vakulinina <lerrun@gmail.com>
2021-03-10 18:08:44 +0300

    feat: add typical arrays problems code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || !array.length) return 0;
+  return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || !array.length) return 0;
+  return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || !array.length) return 0;
+  return array.reduce((acc, value) => acc + value, 0) / array.length;
 }
